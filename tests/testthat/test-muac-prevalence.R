a88a1f5f6109332b1a035eb7db8ff74830307662
Ernest Guevarra <ernest@guevarra.io>
2024-02-22 04:49:14 +0000

    add tests

--- tests/testthat/test-muac-prevalence.R
+++ tests/testthat/test-muac-prevalence.R
@@ -7,3 +7,15 @@ muac_prevalence <- ipc_muac_check(
 
 
 testthat::expect_type(muac_prevalence, "double")
+
+
+muac_prevalence <- muac_data |>
+  dplyr::mutate(sex = ifelse(sex == 1, "m", "f")) |>
+  ipc_muac_check(
+    sex_recode = c("m", "f"), muac_units = "cm",
+    oedema_recode = c(1, 2), .summary = FALSE
+  ) |>
+  ipc_calculate_prevalence(status = "mam")
+
+
+testthat::expect_type(muac_prevalence, "double")
